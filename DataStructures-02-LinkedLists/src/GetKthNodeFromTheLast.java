099d483dec7d942c2bb77055a84e00ab22d64416
Fatih-Cyber <71564472+Fatih-Cyber@users.noreply.github.com>
2020-11-06 17:42:32 +0300

    Data Structures and Algorithms Linked Projects Assignments: 1. Arrays    a. Rotate an n x n Matrix 90 degrees CW in-place. 2. Linked Lists    a. Reverse Nodes of Linked List,    b. Get the Kth node from the last where you dont know the size of the linked list,    c. Print the middle node/nodes of a linked list where you dont know the size of the linked list. 3. Stacks with Linked List Implementation

--- DataStructures-02-LinkedLists/src/GetKthNodeFromTheLast.java
+++ DataStructures-02-LinkedLists/src/GetKthNodeFromTheLast.java
@@ -5,6 +5,7 @@ public class GetKthNodeFromTheLast {
         for(int j=1; j<=10;j++) myLinkedList.addLast(j);
         System.out.println("Linked List----------------:");
         myLinkedList.printLinkedList();
+        System.out.println(5+"th Node from the last is :" );
         System.out.println(myLinkedList.getKthFromTheEnd(5));
     }
 }
